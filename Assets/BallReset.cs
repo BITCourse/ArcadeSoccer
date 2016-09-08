6915e7013504d0af2e8b90a7c4f3c7f890fff45d
LuiCat <zhtl1995@163.com>
2016-09-08 22:18:48 +0800

    add ball reset

---
+++ Assets/BallReset.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallReset : MonoBehaviour {
+
+    public Vector3 resetPosition = new Vector3();
+
+    public GameObject showWhenGoal;
+
+    private bool resetInProgress = false;
+    private float deadTime = -20;
+
+    private Rigidbody rigid;
+    private Collider coll;
+
+    // Use this for initialization
+    void Start () {
+        Score.Instance().onScored += onScored;
+        rigid = GetComponent<Rigidbody>();
+        coll = GetComponent<Collider>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (resetInProgress)
+        {
+            GetComponent<Renderer>().material.color = new Color(1, 1, 1,
+                Mathf.Max(0.65f - (Time.time - deadTime) * 0.3f, 0.0f));
+        }
+
+    }
+
+    void onScored()
+    {
+        if (resetInProgress)
+            return;
+        resetInProgress = true;
+        deadTime = Time.time;
+        rigid.useGravity = false;
+        rigid.velocity *= 0.3f;
+        showWhenGoal.SetActive(true);
+        Invoke("resetBall", 2.5f);
+    }
+
+    void resetBall()
+    {
+        transform.position = resetPosition;
+        rigid.velocity = Vector3.zero;
+        rigid.useGravity = true;
+        showWhenGoal.SetActive(false);
+        GetComponent<Renderer>().material.color = new Color(1, 1, 1, 0.65f);
+        resetInProgress = false;
+    }
+
+}
