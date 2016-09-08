d0eea4a623f2ff189be64ef7a1dc872590e8739d
LuiCat <zhtl1995@163.com>
2016-09-08 14:11:09 +0800

    add charactor

---
+++ Assets/BounceBallBehavior.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceBallBehavior : MonoBehaviour {
+
+    Rigidbody rigid = null;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if(collision.gameObject.tag == "Player")
+        {
+            Vector3 dir = transform.position - collision.gameObject.transform.position;
+            dir.Normalize();
+            dir *= collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude * 0.5f;
+            dir.y *= 0.5f;
+            rigid.velocity += dir;
+        }
+    }
+
+
+}
---
+++ Assets/CharactorController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CharactorController : MonoBehaviour {
+
+    public Transform mainCamera;
+
+    public float cameraDistance = 1f;
+    public float cameraHeight = 1f;
+
+    public float mouseXSensitivity = 0.02f;
+    public float mouseYSensitivity = 0.02f;
+
+    public float moveSpeed = 0.1f;
+
+    public float jumpSpeed = 5f;
+
+    private float charRot = 0;
+    private float bodyTurn = 0;
+    private Vector2 targetViewRotation = new Vector2(0, 0);
+    private Rigidbody rigid = null;
+
+    private Collider col = null;
+
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody>();
+        col = GetComponents<Collider>()[0];
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main.transform;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // 处理控制器
+        float deltaX = CrossPlatformInputManager.GetAxis("Horizontal");
+        float deltaZ = CrossPlatformInputManager.GetAxis("Vertical");
+        bool jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        bool fire1 = CrossPlatformInputManager.GetButtonDown("Fire1");
+        float xRot = CrossPlatformInputManager.GetAxis("Mouse X") * mouseXSensitivity;
+        float yRot = CrossPlatformInputManager.GetAxis("Mouse Y") * mouseYSensitivity;
+
+        // 左右横移转身
+        float targetTurn = 0;
+        if (deltaX < 0) targetTurn = -30.0f;
+        else if (deltaX > 0) targetTurn = 30.0f;
+
+        bodyTurn += (targetTurn - bodyTurn) * (1 - Mathf.Exp(-Time.deltaTime * 10f));
+        
+        // 转动人物
+        charRot = targetViewRotation.x + bodyTurn;
+        transform.localRotation = Quaternion.Euler(0, charRot, 0);
+
+        // 移动人物
+        /*transform.localPosition += Quaternion.Euler(0, targetViewRotation.x, 0) 
+            * new Vector3(deltaX * moveSpeed, 0, deltaZ * moveSpeed);*/
+        float velY = rigid.velocity.y;
+        if (jump && transform.position.y < 0.1)
+        {
+            velY = jumpSpeed;
+        }
+        rigid.velocity = Quaternion.Euler(0, targetViewRotation.x, 0)
+            * new Vector3(deltaX * moveSpeed, velY, deltaZ * moveSpeed);
+
+        // 视角转动
+        targetViewRotation.x = targetViewRotation.x + xRot;
+        targetViewRotation.y = targetViewRotation.y - yRot;
+        if (targetViewRotation.y > 90.0f) targetViewRotation.y = 90.0f;
+        if (targetViewRotation.y < -90.0f) targetViewRotation.y = -90.0f;
+
+        // 移动相机
+        mainCamera.localRotation = Quaternion.Euler(targetViewRotation.y, targetViewRotation.x, 0);
+        mainCamera.localPosition = transform.localPosition + new Vector3(0, cameraHeight, 0)
+            + mainCamera.localRotation * new Vector3(0, 0, -cameraDistance);
+
+    }
+}
