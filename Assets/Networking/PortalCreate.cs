13a1f486dbe4dcf2a3d4651fc9713069f826d2ca
Yeehok <yeehok@gmail.com>
2016-09-17 17:31:37 +0800

    添加地图互动，传送门

---
+++ Assets/Networking/PortalCreate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PortalCreate : NetworkBehaviour
+{
+    public GameObject Portal;
+
+    public override void OnStartServer()
+    {
+        var portal = (GameObject)Instantiate(Portal);
+        NetworkServer.Spawn(portal);
+    }
+}
---
+++ Assets/Portal.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour
+{
+    public Transform portalOut;
+    float yOffset = 3f;
+
+    public GameObject portalEffect;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ball")
+        {
+            Vector3 newPos = other.GetComponent<Rigidbody>().position + (portalOut.position - transform.position);
+            newPos.y = portalOut.position.y + yOffset;
+            other.GetComponent<Rigidbody>().position = newPos;
+            portalEffect.SetActive(false);
+            portalEffect.SetActive(true);
+        }
+        else if (other.tag == "Player")
+        {
+            other.GetComponent<Rigidbody>().position = portalOut.position;
+            portalEffect.SetActive(false);
+            portalEffect.SetActive(true);
+        }
+    }
+
+}
